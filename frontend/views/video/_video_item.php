54132b1285fbd43d2cc29326ab791e2ce85a6f83
Ilya Rogachev <rogachev.ilya@list.ru>
2022-06-03 12:38:44 +0700

    output published videos on frontend

--- frontend/views/video/_video_item.php
+++ frontend/views/video/_video_item.php
@@ -1,1 +1,16 @@
 <?php
+/** @var $model \common\models\Video */
+?>
+
+<div class="card m-3" style="width: 18rem;">
+    <div class="embed-responsive embed-responsive-16by9 mb-3">
+        <video src="<?php echo $model->getVideoLink() ?>"
+               poster="<?php echo $model->getThumbnailLink() ?>"></video>
+    </div>
+
+    <div class="card-body p-2">
+        <h5 class="card-title m-0"><?php echo  $model->title ?></h5>
+        <p class="text-muted m-0"><?php echo $model->created_by->username ?></p>
+        <p class="text-muted card-text m-0">8 просмотров • <?php echo Yii::$app->formatter->asRelativeTime($model->created_at) ?></p>
+    </div>
+</div>
--- frontend/views/video/index.php
+++ frontend/views/video/index.php
@@ -4,5 +4,9 @@
 
 <?php echo \yii\widgets\ListView::widget([
     'dataProvider' => $dataProvider,
-    'itemView' => '_video_item'
+    'itemView' => '_video_item',
+    'layout' => '<div class="d-flex flex-wrap">{items}</div>{pager}',
+    'itemOptions' => [
+        'tag' => false,
+    ]
 ])?>
